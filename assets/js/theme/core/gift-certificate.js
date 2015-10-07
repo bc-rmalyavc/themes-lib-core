2e0b43d47aece01a7b1b63bdd31dab7b3218570e
essmahr <essmahr@gmail.com>
2015-10-07 11:31:35 -0700

    Add gift certificate markup.
    add jQuery import
    
    gift_page to page
    
    tidy up namespacing

---
+++ assets/js/theme/core/gift-certificate.js
@@ -0,0 +1,25 @@
+import PageManager from '../../page-manager';
+import $ from 'jquery';
+
+export default class GiftCertificate extends PageManager {
+  constructor() {
+    super();
+    this.$purchaseForm = $('[data-gift-certificate-purchase-form]');
+  }
+
+  loaded() {
+    if (this.$purchaseForm.length) {
+      this._bindPreview();
+    }
+  }
+
+  _bindPreview() {
+    // TODO: validation
+    $('[data-preview-url]').on('click', (event) => {
+      const previewUrl = $(event.currentTarget).data('preview-url') + '&' + this.$purchaseForm.serialize();
+      event.preventDefault();
+      window.open(previewUrl);
+    });
+  }
+}
+
