eb5027617c1f95ee90d89c84e739f0bc4eea4b92
Margaret Gauthier <margaret@pixelunion.net>
2017-05-04 16:08:13 -0700

    Update core to support optimized checkout

--- install-lang.php
+++ install-lang.php
@@ -5,18 +5,26 @@
  * All translations are placed in a `core` object.
  */
 
-$src_path = __DIR__ . '/lang/en.json';
-$theme_path = realpath($argv[1]) . '/lang/en.json';
+function merge_json($src_path, $dest_path) {
+  $src_contents = file_get_contents($src_path);
+  $src_json = json_decode($src_contents, true);
+  echo 'core  ', strlen($src_contents), ' ', $src_path, PHP_EOL;
 
-// Get core lang
-$src_json = file_get_contents($src_path);
-$src_lang = json_decode($src_json);
-echo 'core  ', strlen($src_json), ' ', $src_path, PHP_EOL;
+  $dest_contents = file_get_contents($dest_path);
+  $dest_json = json_decode($dest_contents, true);
+  echo 'theme  ', strlen($dest_contents), ' ', $dest_path, PHP_EOL;
 
-// Get theme lang
-$theme_json = file_get_contents($theme_path);
-$theme_lang = json_decode($theme_json);
-echo 'theme ', strlen($theme_json), ' ', $theme_path, PHP_EOL;
+  // var_dump($dest_contents);
+  // var_dump($dest_json);
+  $merged = array_merge($dest_json, $src_json);
+  print_r($merged);
+}
+
+$theme_path = realpath($argv[1]);
+
+// Merge language files
+merge_json(__DIR__ . '/lang/en.json', $theme_path . '/lang/en.json');
+return;
 
 // Update theme lang
 $theme_lang->core = $src_lang->core;
